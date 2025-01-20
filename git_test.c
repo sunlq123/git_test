193e1a63a16fece6b5deefa8e3138650c8498aa5
sunlq123 <liangmengqwe@gmail.com>
2025-01-20 10:58:11 +0800

    second commit

--- git_test.c
+++ git_test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("hello, git!\n");
+    char name[20] = {0};
+    printf("hello, git!\n"); // 提交一次后再次修改文件
+    puts("what is your name: ");
+    scanf("%s", name);
+    printf("hello, %s", name);
     return 0;
 }
