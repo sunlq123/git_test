ac609ee8ae626ca452acf16d102dc6508735105e
sunlq123 <liangmengqwe@gmail.com>
2025-01-20 15:32:27 +0800

    six commit

--- git_test.c
+++ git_test.c
@@ -3,10 +3,13 @@ int main(void)
 {
     //蓝色的线表示这个存档是基于上个存档演变来的
     char name[20] = {0};
-    int two = 1;
+    int number;
     printf("hello, git!\n"); // 提交一次后再次修改文件
     puts("what is your name: ");
     scanf("%s", name);
     printf("hello, %s", name);
+    puts("input a number:");
+    scanf("%d", &number);
+    printf("$%d", number);
     return 0;
 }
